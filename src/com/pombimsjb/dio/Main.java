8ca1bcf3968e33df51e646c94a907b18c8dec2d4
GiancarloMolinari <itachisolid@gmail.com>
2023-03-28 23:18:51 -0300

    Inicio Projeto

--- src/com/pombimsjb/dio/Main.java
+++ src/com/pombimsjb/dio/Main.java
@@ -51,23 +51,34 @@ public class Main {
         System.out.println("i2 > f1 " + (i2 > f1));
         System.out.println("d1 == h1 " + (d1 == h1));
         //System.out.println(s2 != c1);
-       // System.out.println(s3 != i1);
+        // System.out.println(s3 != i1);
         System.out.println("\n");
         System.out.println("l1 == l2 " + (l1 == l2));
         System.out.println("l2 >= l1 " + (l2 >= l1));
         System.out.println("y1 != h1 " + (y1 != h1));
         System.out.println("\nOperadores Lógicos");
 
-        System.out.println("b1 && b2 "+(b1 && b2));
-        System.out.println("b1 && b3 "+(b1 && b3));
-        System.out.println("b2 || b3 "+(b2 || b3));
-        System.out.println("b2 || b4 "+(b2 || b4));
-        System.out.println("b1 ^ b3 "+(b1 ^ b3));
-        System.out.println("b4 ^ b1 "+(b4 ^ b1));
-        System.out.println("!b1 "+(!b1));
-        System.out.println("!b2 "+(!b2));
-        System.out.println("(i1>i2)||(f2<f1) "+((i1>i2)||(f2<f1)));
-        System.out.println("(i1+i2)<(f2-f1)&&true "+((i1+i2)<(f2-f1)&&true));
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));
+        System.out.println("!b1 " + (!b1));
+        System.out.println("!b2 " + (!b2));
+        System.out.println("(i1>i2)||(f2<f1) " + ((i1 > i2) || (f2 < f1)));
+        System.out.println("(i1+i2)<(f2-f1)&&true " + ((i1 + i2) < (f2 - f1) && true));
+
+        double salarioMensal = 11893.58d;
+        double mediaSalario = 10500d;
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
+        System.out.println((salarioBaixo) && (muitosDependentes));
+        boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebeAuxilo " + recebeAuxilio);
 
     }
 }
